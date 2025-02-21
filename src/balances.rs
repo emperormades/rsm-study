9754cc7d94eac8ad4290b840056a7290fbeab8fc
mades eladrian <mades.eladrian@gmail.com>
2025-02-21 20:24:42 -0300

    refact to a generic Pallet

--- src/balances.rs
+++ src/balances.rs
@@ -1,14 +1,16 @@
 use std::collections::BTreeMap;
-
-type Balance = u128;
-type AccountId = String;
+use num::traits::{CheckedAdd, CheckedSub, Zero};
 
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, Balance> {
     balances: BTreeMap<AccountId, Balance>
 }
 
-impl Pallet {
+impl<AccountId, Balance> Pallet<AccountId, Balance>
+where
+    AccountId: Ord + Clone,
+    Balance: CheckedAdd + CheckedSub + Zero + Copy
+{
     pub fn new() -> Self {
         Pallet {
             balances: BTreeMap::new()
@@ -20,7 +22,7 @@ impl Pallet {
     }
 
     pub fn balance(&self, account: &AccountId) -> Balance {
-        *self.balances.get(account).unwrap_or(&0)
+        *self.balances.get(account).unwrap_or(&Balance::zero())
     }
 
     /// Transfer `amount` from one account to another.
@@ -36,11 +38,11 @@ impl Pallet {
         let to_balance = self.balance(&to);
 
         let new_caller_balance = caller_balance
-            .checked_sub(amount)
+            .checked_sub(&amount)
             .ok_or("Insufficient balance")?;
 
         let new_to_balance = to_balance
-            .checked_add(amount)
+            .checked_add(&amount)
             .ok_or("Overflow")?;
 
         self.balances.insert(caller, new_caller_balance);
